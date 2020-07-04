2c415805f23abab51469746734f9883cfe6415ae
gilangrusydi <justgilang00@gmail.com>
2020-07-04 14:27:37 +0700

    add: content

--- js/Portfolio.js
+++ js/Portfolio.js
@@ -18,35 +18,10 @@ class Portfolio{
             if(this.getAttribute('state') == 'more'){
                 this.setAttribute('state', 'less');
                 this.innerHTML = '👆less';
-                
-                //Set bar animation
-                setTimeout(()=>{
-                    portfolio.runBarAnimation();
-                },300);
-
             }else{
                 this.setAttribute('state', 'more');
                 this.innerHTML = '👇more';
-                
-                //remove bar 
-                setTimeout(()=>{
-                    portfolio.removeBar();
-                },100);
             }
-        });
-    }
-
-    //run bar animation
-    runBarAnimation(){
-        [...$('.progress-value')].forEach(bar =>{
-            bar.style.width = `${bar.getAttribute('data-value')}%`;
-        });
-    }
-
-    //remove bar value
-    removeBar(){
-        [...$('.progress-value')].forEach(bar =>{
-            bar.removeAttribute('style');
         });
     }
 
